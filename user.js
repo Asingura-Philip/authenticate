4f209286fed1a464d2e548aeba5529c27f13e846
Asingura Philip <asinguraphilip@gmail.com>
2025-02-27 17:25:34 +0300

    user file

--- user.js
+++ user.js
@@ -4,8 +4,8 @@ const usersFilePath = path.join(__dirname, 'users.json');
 
 // Ensure users.json exists
 function ensureUsersFileExists(callback) {
-  fs.exists(usersFilePath, (exists) => {
-    if (!exists) {
+  fs.access(usersFilePath, fs.constants.F_OK, (err) => {
+    if (err) {
       fs.writeFile(usersFilePath, JSON.stringify([]), (err) => {
         if (err) throw err;
         callback();
@@ -16,12 +16,11 @@ function ensureUsersFileExists(callback) {
   });
 }
 
-// Save user data to users.json
+// Save user data
 function saveUser(userData, callback) {
   ensureUsersFileExists(() => {
     fs.readFile(usersFilePath, (err, data) => {
       if (err) throw err;
-
       let users = JSON.parse(data);
       users.push(userData);
 
@@ -37,11 +36,31 @@ function saveUser(userData, callback) {
 function getUserByEmail(email, callback) {
   fs.readFile(usersFilePath, (err, data) => {
     if (err) throw err;
-
     let users = JSON.parse(data);
     const user = users.find(u => u.email === email);
-    callback(user);
+    callback(user || null);
   });
 }
 
-module.exports = { saveUser, getUserByEmail };
+// Update user profile
+function updateUserProfile(email, newName, newEmail, callback) {
+  fs.readFile(usersFilePath, (err, data) => {
+    if (err) throw err;
+    let users = JSON.parse(data);
+    let user = users.find(u => u.email === email);
+
+    if (user) {
+      user.name = newName;
+      user.email = newEmail;
+
+      fs.writeFile(usersFilePath, JSON.stringify(users, null, 2), (err) => {
+        if (err) throw err;
+        callback('Profile updated successfully!');
+      });
+    } else {
+      callback('User not found.');
+    }
+  });
+}
+
+module.exports = { saveUser, getUserByEmail, updateUserProfile };
